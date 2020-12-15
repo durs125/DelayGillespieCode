84c7c09f2addf53a6b9818928898308f828d56cf
David Infortunio <durs125@yahoo.com>
2020-12-15 10:55:24 -0500

    Update Module3.py

--- ConsolidatedCode/Module3.py
+++ ConsolidatedCode/Module3.py
@@ -23,22 +23,31 @@ import matplotlib.pyplot as plt
 # add ,filterParamiters)
 
 
-def run_pipeline_splits(initialize_Gillespie, reactionList, runVector, path1):
+def run_pipeline_splits(initialize_Gillespie,  runVector, path1):
     # Prepare the reactions classes.
-    [alpha, beta, yr, r0, c0, mu, cv, initialState] = initialize_Gillespie
+    [alpha, beta, yr, r0, c0, mu, cv, initialState, run_count ] = initialize_Gillespie [0:9]
     initialState = int (initialState)
     [length_run, split_Size, burnInTime, sampleRate] = runVector
-    dilution = Gill.Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])
-    enzymatic_degradation = Gill.Reaction(np.array([-1], dtype=int), 0, 0, [0, yr, r0, 1], 1, [0])
-    production = Gill.Reaction(np.array([1], dtype=int), 0, 1, [alpha, c0, 2], 0, [mu, mu * cv])
+    [stopTime, runCount, burnInTime, sampleRate] = runVector
+    # print(run_count)
+    dilution = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])
 
+    enzymatic_degradation = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, yr, r0, 1], 1, [0])
+    print("degrade")
+    production = Classy.Reaction(np.array([1], dtype=int), 0, 1, [alpha, c0, 2], 0, [mu, mu * cv])
+
+    reactionList = np.array([production, enzymatic_degradation, dilution])
     temp_signalcsv = path1 + 'temp_signal.csv'
     temp_peakscsv = path1 + "temp_peaks.csv"
     statscsv = path1 + "stats.csv"
 
-    tempOutFileSignal = open('temp_signal.csv', 'a')
-    tempOutFilePeaks = open("temp_peaks.csv", 'a')
-    OutFileStats = open("stats.csv", 'a')
+    temp_signalcsv = path1 + '{}temp_signal.csv'.format( initialize_Gillespie )
+    temp_peakscsv = path1 + "{}temp_peaks.csv".format( initialize_Gillespie )
+    statscsv = path1 + "{}stats.csv".format( initialize_Gillespie )
+
+    tempOutFileSignal = open(temp_signalcsv, 'a')
+    tempOutFilePeaks = open(temp_peakscsv, 'a')
+    OutFileStats = open(statscsv, 'a')
 
     header = "alpha, beta, yr, r0, c0, mu, cv, initialState, peaks"
     np.savetxt("temp_peaks.csv", header, delimiter=",")
@@ -73,11 +82,13 @@ def run_pipeline_splits(initialize_Gillespie, reactionList, runVector, path1):
 def run_pipeline_single(initialize_Gillespie,  runVector, path1):
     # Prepare the reactions classes.
     #[alpha, beta, yr, r0, c0, mu, cv, initialState, run_count, production, enzymatic_degradation, dilution ] = initialize_Gillespie
-    [alpha, beta, yr, r0, c0, mu, cv, initialState, run_count ] = initialize_Gillespie
+    print(initialize_Gillespie)
+    [alpha, beta, yr, r0, c0, mu, cv, initialState, run_count ] = initialize_Gillespie [0:9] #DMI
+    #[[alpha, beta, yr, R0, C0, mu, cv, initialState, run]]
     initialState = np.array([initialState], dtype=int)
-    params = [alpha, beta, yr, r0, c0, mu, cv, int(initialState)]
+    #params = [alpha, beta, yr, r0, c0, mu, cv, int(initialState), run_count] #DMI
     [stopTime, runCount, burnInTime, sampleRate] = runVector
-    print(params)
+    #print(run_count)
     dilution = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])
 
     enzymatic_degradation = Classy.Reaction(np.array([-1], dtype=int), 0, 0, [0, yr, r0, 1], 1, [0])
@@ -87,13 +98,13 @@ def run_pipeline_single(initialize_Gillespie,  runVector, path1):
     reactionList = np.array([production, enzymatic_degradation, dilution])
 
 
-    temp_signalcsv = path1 + 'temp_signal.csv'
-    temp_peakscsv = path1 + "temp_peaks.csv"
-    statscsv = path1 + "stats.csv"
+    temp_signalcsv = path1 + '{}temp_signal.csv'.format( initialize_Gillespie )
+    temp_peakscsv = path1 + "{}temp_peaks.csv".format( initialize_Gillespie )
+    statscsv = path1 + "{}stats.csv".format( initialize_Gillespie )
 
-    tempOutFileSignal = open('temp_signal.csv', 'a')
-    tempOutFilePeaks = open("temp_peaks.csv", 'a')
-    OutFileStats = open("stats.csv", 'a')
+    tempOutFileSignal = open(temp_signalcsv, 'a')
+    tempOutFilePeaks = open(temp_peakscsv, 'a')
+    OutFileStats = open(statscsv, 'a')
 
     # header = "alpha, beta, yr, r0, c0, mu, cv, initialState, peaks, run_count"
     # np.savetxt("temp_peaks.csv", header, delimiter=",")
@@ -101,13 +112,13 @@ def run_pipeline_single(initialize_Gillespie,  runVector, path1):
 
     tempSignal = Gill.gillespie(reactionList, stopTime, initialState)
     np.zeros(min(tempSignal.shape[1],9))
-    np.savetxt("temp_signal.csv", params, delimiter=",")
-    np.savetxt("temp_signal.csv", tempSignal, delimiter=",")
+    np.savetxt(temp_signalcsv, initialize_Gillespie, delimiter=",")
+    np.savetxt(temp_signalcsv, tempSignal, delimiter=",")
     print(tempSignal)
     burned_in_signal = Post.burn_in_time_series(tempSignal, burnInTime)  # this needs to be passed by reference.
     uniform_sampling = Post.uniformly_sample(burned_in_signal, sampleRate)  # this needs to be passed by reference.
     peaks = Post.detect_peaks(uniform_sampling)
-    #to_peak_file = initialize_Gillespie.extend(peaks)  # DMI  FIX!, issue: the peaks are in the form of time and height.
+    to_peak_file = initialize_Gillespie.extend(peaks)  # DMI  FIX!, issue: the peaks are in the form of time and height.
     #   thing = np.zeros(peaks.shape[1]))
     #    thing[:] = ' '.join(map(str, my_lst))
     #   to_peak_file[len(to_peak_file):] =
@@ -115,12 +126,13 @@ def run_pipeline_single(initialize_Gillespie,  runVector, path1):
 
     # recommended fix in format of making the peak data have a different set of deliminators than the other data
     # other fix, just put the parameters near the peak amp and time, along with a slice number
-    np.savetxt("temp_peaks.csv", to_peak_file, delimiter=",")  # might want to try json, might work
+    #np.savetxt("temp_peaks.csv", to_peak_file, delimiter=",")  # might want to try json, might work
     # know np.save(filename.npy,list) can save list to file. Cannot append multiple files
     to_stat_file = initialize_Gillespie
     to_stat_file[len(to_stat_file):] = Post.run_statistics(peaks)
     to_stat_file = Post.run_statistics(peaks)  # send the stats here
-    np.savetxt("stats.csv", to_stat_file, delimiter=",")
+    print(peaks)
+    np.savetxt(statscsv, to_stat_file, delimiter=",")
 
     tempOutFileSignal.close()
     tempOutFilePeaks.close()
